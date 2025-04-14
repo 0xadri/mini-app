0fcb76e4ddad9cbb2cfaa1a28a54fd7756343d73
0xadri <adiber_garcia@hotmail.com>
2025-04-14 18:18:42 +0200

    Favorite Actors Page: complete

--- mini-app/src/pages/FavActors.jsx
+++ mini-app/src/pages/FavActors.jsx
@@ -1,5 +1,19 @@
+import ActorCard from "../components/ActorCard";
+import { useFavActorsContext } from "../contexts/FavActorsContext";
+import { useState, useEffect } from "react";
+
 const FavActors = () => {
-  return <h1>Favorite Actors</h1>;
+  const { favoriteActors } = useFavActorsContext();
+
+  return (
+    <>
+      <h1>Favorite Actors</h1>
+      {favoriteActors &&
+        favoriteActors.map((actor, index) => (
+          <ActorCard key={index} actor={actor} />
+        ))}
+    </>
+  );
 };
 
 export default FavActors;
