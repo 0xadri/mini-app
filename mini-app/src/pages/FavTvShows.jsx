86f2c918598bbc8920699a0f91b62feb98a78737
0xadri <adiber_garcia@hotmail.com>
2025-04-15 11:53:13 +0200

    Fav Movies Page: rm deprecated code

--- mini-app/src/pages/FavTvShows.jsx
+++ mini-app/src/pages/FavTvShows.jsx
@@ -1,7 +1,21 @@
+import TvshowCard from "../components/TvshowCard";
 import { useFavTvShowsContext } from "../contexts/FavTvShowsContext";
 
 const FavTvShows = () => {
-  return <h1>Favorite TV Shows</h1>;
+  const { favTvShows } = useFavTvShowsContext();
+
+  return (
+    <>
+      <h1>Favorite TV Shows</h1>
+      {favTvShows ? (
+        favTvShows.map((tvshow) => {
+          return <TvshowCard tvshow={tvshow} />;
+        })
+      ) : (
+        <div>Nothing</div>
+      )}
+    </>
+  );
 };
 
 export default FavTvShows;
